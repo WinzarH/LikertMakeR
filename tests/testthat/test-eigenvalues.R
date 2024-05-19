19e86ff6e416415d883a613443ef882744216d1d
Hume Winzar <winzar@gmail.com>
2024-05-19 19:49:08 +1000

    restyle ready fro CRAN

--- tests/testthat/test-eigenvalues.R
+++ tests/testthat/test-eigenvalues.R
@@ -1,17 +1,17 @@
 ## Unit tests for eigenvalues() function
 
 test_that("eigenvalues runs silently with valid inputs", {
- ## define parameters
+  ## define parameters
 
- correlationMatrix <- matrix(
-   c(
-     1.00, 0.25, 0.35, 0.40,
-     0.25, 1.00, 0.70, 0.75,
-     0.35, 0.70, 1.00, 0.80,
-     0.40, 0.75, 0.80, 1.00
-   ),
-   nrow = 4, ncol = 4
- )
+  correlationMatrix <- matrix(
+    c(
+      1.00, 0.25, 0.35, 0.40,
+      0.25, 1.00, 0.70, 0.75,
+      0.35, 0.70, 1.00, 0.80,
+      0.40, 0.75, 0.80, 1.00
+    ),
+    nrow = 4, ncol = 4
+  )
 
 
   ## apply function examples
@@ -19,6 +19,4 @@ test_that("eigenvalues runs silently with valid inputs", {
   expect_no_warning(eigenvalues(cormatrix = correlationMatrix))
 
   expect_no_warning(eigenvalues(correlationMatrix, 1))
-
 })
-
